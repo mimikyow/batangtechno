e550d705f3769b4a0cc4eccfbd1ca60a89931252
VINCE CLYDE BANAAG <22-06373@g.batstate-u.edu.ph>
2026-03-21 06:29:59 +0000

    the "Batang Techno | Hackathon Judging Platform". i want to change it to

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
-  title: 'Batang Techno | Mission Command',
+  title: 'Batang Techno',
   description: 'A futuristic judging platform for local hackathons',
 };
 
