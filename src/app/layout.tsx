2ecee909f7cdbe6a9f3749671e09147e4767dbfe
VINCE CLYDE BANAAG <22-06373@g.batstate-u.edu.ph>
2026-03-21 06:28:28 +0000

    where in the code do you change the website title displayed in the tab?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
-  title: 'Batang Techno | Hackathon Judging Platform',
+  title: 'Batang Techno | Mission Command',
   description: 'A futuristic judging platform for local hackathons',
 };
 
